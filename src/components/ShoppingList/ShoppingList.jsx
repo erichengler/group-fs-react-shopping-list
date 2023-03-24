d29ac59f26fd2223eb57f9b8ead62902c7f54df1
Erich Engler <erich.engler@hotmail.com>
2023-03-24 13:38:33 -0500

    added glonel's css

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -65,21 +65,20 @@ function ShoppingList() {
 
         <div>
             <h1>Shopping List</h1>
-            <button onClick={clearAll}>Clear</button>
+            <button onClick={clearAll} >Clear</button>
             <button onClick={resetAll}>Reset</button>
-                <ul>
+            <br></br>
+                <div className="itemGrid">
                     {
                         itemArray.map((item) => (
                             <ShoppingItem 
                                 key={item.id}
                                 item={item}
                                 fetchItemList={fetchItemList}
-                            
                             />
-
                         ))
                     }
-                </ul>
+                </div>
         </div>
         </>
     )
